e95be8c315b2b9ce876b3c9357cb8465693f47cb
dougdechow <32960293+dougdechow@users.noreply.github.com>
2022-05-06 15:53:37 -0700

    Source file for a Basic Text Processing in R tutorial.
    The source code in this R file comes from the Programming Historian tutorial Basic Text Processing in R: https://programminghistorian.org/en/lessons/basic-text-processing-in-r

---
+++ ph-basic-text-processing.R
@@ -0,0 +1,17 @@
+#
+# Proj: Basic Text Processing in R
+# Desc: Learn how to use R to analyze high-level patterns in texts, 
+#       apply stylometric methods over time and across authors, and 
+#       use summary methods to describe items in a corpus.
+#       This source code is from a Programming Historian tutorial:
+#       https://programminghistorian.org/en/lessons/basic-text-processing-in-r
+# Date: 2022-05-06
+#
+
+# Package Set-up 
+# Two R packages need to be installed for the tutorial: tidyverse3 and tokenizers
+install.packages("tidyverse")
+install.packages("tokenizers")
+
+install.packages("tidyverse")
+install.packages("tokenizers")
